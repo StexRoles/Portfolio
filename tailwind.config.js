669730a9434a6b1b23c653de0c3fe40bceaf4a79
StexRoles <navarro456123@gmail.com>
2024-06-21 18:43:03 -0500

    creating name animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,21 @@ export default {
         'smoke-white': '#F2F3F4',
         'card': '#566573'
       },
+      keyframes: {
+        shimmer: {
+          '0%, 100%': { backgroundPosition: '200% center'},
+          '50%': { backgroundPosition: '-100% center'},
+        }
+      },
+      animation: {
+        shimmer: 'shimmer 10s linear infinite'
+      },
+      backgroundImage: {
+        'shimmer': 'linear-gradient(90deg, rgba(242,243,244,1) 0%, rgba(242,243,244,1) 25%, rgba(133,146,158,1) 50%, rgba(242,243,244,1) 74%, rgba(242,243,244,1) 100%)',
+      },
+      backgroundSize: {
+        'shimmer-size': '200% 100%',
+      },
     },
   },
   plugins: [],
